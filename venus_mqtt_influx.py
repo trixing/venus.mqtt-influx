e0cc8983167e8d47d5c4a7d715944e86010224aa
Jan Dittmer <jdi@l4x.org>
2025-04-10 21:06:07 +0000

    ignore unchanged values

--- venus_mqtt_influx.py
+++ venus_mqtt_influx.py
@@ -261,12 +261,15 @@ class MqttToInflux:
 
    def write(self):
       deduped = 0
+      unchanged = 0
       points = defaultdict(list)
       agg = defaultdict(dict)
+      changed = dict()
       timer = datetime.utcnow()
       timer = timer - timedelta(seconds=timer.second % INTERVAL,
                              microseconds=timer.microsecond)
       timer = timer + timedelta(seconds=INTERVAL)
+      unchanged_timer = timer + timedelta(hours=1)
       while self._active:
         now = time.time()
         try:
@@ -302,7 +305,13 @@ class MqttToInflux:
             points[k].append(p)
         except queue.Empty:
             pass
+
         now = datetime.utcnow()
+        if unchanged_timer <= now:
+            unchanged_timer = timer + timedelta(hours=1)
+            changed = dict()
+            log.info('Flush unchanged cache')
+
         if timer <= now:
             # this is slightly wrong and should be corrected by INTERVAL/2
             # also it would be nice to run this on a full 10s interval
@@ -312,6 +321,7 @@ class MqttToInflux:
             if points:
                 tbw = []
                 duped = 0
+                unchanged = 0
                 for k, ms in points.items():
                     # These are sampled on every datapoint
                     #if '.Power.' in k or 'Dc.0.Current' in k or 'Dc.0.Voltage' in k:
@@ -324,12 +334,17 @@ class MqttToInflux:
                     elif ms[0]['fields'].get('text', None) is not None:
                         # Don't need to do anything, just take the first value
                         # TODO(jdi): Should be mode probably.
+                        value = ms[0]['fields'].get('text', None)
                         pass
                     duped += len(ms) - 1
                     ms[0]['time'] = dt
-                    tbw.append(ms[0])
-                log.info('Write %d points (across %d unique measurements), Deduped %d, Interval %.3fs' % (
-                    len(tbw), len(points), duped, interval.total_seconds()))
+                    if k in changed and changed[k] == value:
+                      unchanged += 1
+                    else:
+                      tbw.append(ms[0])
+                      changed[k] = value
+                log.info('Write %d points (across %d unique measurements), Deduped %d, Unchanged %d, Interval %.3fs' % (
+                    len(tbw), len(points), duped, unchanged, interval.total_seconds()))
                 # print(points.keys())
                 if not self._dryrun:
                     latency = time.time()
